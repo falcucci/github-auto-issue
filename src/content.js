f436bc670ea7fdeb5bcc950ca5924e6a8a6e2859
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-02-26 21:56:45 +0100

    update content.js
    updates the `content.js` file to add a "summarize" button and to update the dom manipulation code.
    
    the changes include:
    - adding a `buttonready` variable to check if the "summarize" button is present.
    - updating the `handleclick` function to ensure that the text content is correctly extracted from the dom elements.
    - removing the code for the table element.
    - refactoring the code to ensure that the dom elements are correctly observed and updated.

--- src/content.js
+++ src/content.js
@@ -25,17 +25,15 @@ const button = () => {
 
 const init = async () => {
   await domLoaded;
-  const buttonReady = await elementReady("#summarize");
-  if (!buttonReady) {
-    await elementReady(sucessBtnPath);
-    const successBtn = select(sucessBtnPath);
-    successBtn.append(button());
+  const buttonReady = select("#summarize");
+  const anchor = select(sucessBtnPath);
+  if (!buttonReady && anchor) {
+    anchor.append(button());
   }
 
   const handleClick = async () => {
     await elementReady("#issue_title");
     const titleElement = select("#issue_title");
-    console.log("titleElement: ", titleElement);
     const textContent = titleElement.value;
     await elementReady("#issue_body");
     const issueBodyElement = select("#issue_body");
@@ -54,49 +52,6 @@ const init = async () => {
     },
   });
 
-  await elementReady(".edit-comment-hide");
-
-  const table = content => {
-    return (
-      <table
-        class="d-block user-select-contain"
-        data-paste-markdown-skip="">
-        <tbody class="d-block">
-          <tr class="d-block">
-            <td class="d-block comment-body markdown-body  js-comment-body">
-              <p class="color-fg-muted">{content}</p>
-            </td>
-          </tr>
-          <tr
-            class="d-block pl-3 pr-3 pb-3 js-comment-body-error"
-            hidden="">
-            <td class="d-block">
-              <div class="flash flash-warn" role="alert">
-                <p class="mb-1">
-                  <svg
-                    aria-hidden="true"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    version="1.1"
-                    width="16"
-                    data-view-component="true"
-                    class="octicon octicon-info">
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm6.5-.25A.75.75 0 017.25 7h1a.75.75 0 01.75.75v2.75h.25a.75.75 0 010 1.5h-2a.75.75 0 010-1.5h.25v-2h-.25a.75.75 0 01-.75-.75zM8 6a1 1 0 100-2 1 1 0 000 2z"></path>
-                  </svg>
-                  The text was updated successfully, but these
-                  errors were encountered:
-                </p>
-                <ol class="mb-0 pl-4 ml-4"></ol>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  };
-
   ///////////////////////////////////////////////
   //  this is how we manipulate the component  //
   ///////////////////////////////////////////////
@@ -106,17 +61,11 @@ const init = async () => {
     observe("#issue_body", {
       add: element => {
         if (key === "CHATGPT_OUTPUT") {
-          // const issueDiv = select(
-          //   ".edit-comment-hide .color-fg-muted"
-          // );
-          // issueDiv.textContent = issueDiv.textContent + value.text;
           element.value = element.value + value.text;
         }
       },
     });
   });
-
-  // const stream = await fetchSSE();
 };
 
 init();
